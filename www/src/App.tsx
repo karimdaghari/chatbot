1ca1532b97aad2201155e24cf9c286e5ca58b3ba
karimdaghari <karim_daghari@outlook.com>
2022-05-10 09:50:52 +0100

    style: set distinguishing background

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -2,7 +2,7 @@ import './index.css';
 
 function App() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-screen bg-green-200'>
       <p>Hello!</p>
     </div>
   );
