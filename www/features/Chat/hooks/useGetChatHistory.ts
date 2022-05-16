d927ca294c8fc5bbe047b0d5c88b5af5ed2134c6
karimdaghari <karim_daghari@outlook.com>
2022-05-16 09:30:40 +0100

    fix(www): filter out erroneous responses

--- www/features/Chat/hooks/useGetChatHistory.ts
+++ www/features/Chat/hooks/useGetChatHistory.ts
@@ -1,9 +1,27 @@
+import { compareAsc, parseISO } from 'date-fns';
 import useFetch from 'hooks/useFetch';
+import { useMemo } from 'react';
 import { IChatHistory } from '..';
 
 export default function useGetChatHistory() {
-  return useFetch<IChatHistory[]>({
+  const { data: _data, ...info } = useFetch<IChatHistory[]>({
     key: 'chat',
     endpoint: '/chat'
   });
+
+  const data = useMemo(
+    () =>
+      _data
+        ?.filter(({ text }) => text !== null)
+        ?.map((d) => d)
+        .sort(({ created_at: c1 }, { created_at: c2 }) =>
+          compareAsc(parseISO(c1), parseISO(c2))
+        ),
+    [_data]
+  );
+
+  return {
+    data,
+    ...info
+  };
 }
